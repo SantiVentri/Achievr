416b5aa6db57ae53699a4c893945bc9b85f2d698
Santino Ventrice <santino.ventrice@gmail.com>
2025-06-18 01:28:06 -0300

    Adding Username Update feature

--- app/(home)/editAccount.tsx
+++ app/(home)/editAccount.tsx
@@ -1,11 +1,39 @@
 import Avatar from "@/components/Avatar";
 import { Colors } from "@/constants/palette";
+import { useUser } from "@/context/UserContext";
+import { supabase } from "@/utils/supabase";
 import { MaterialIcons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Alert, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function EditAccountScreen() {
     const router = useRouter();
+    const { user } = useUser();
+    const [username, setUsername] = useState(user?.user_metadata?.display_name || '');
+    const [isLoading, setLoading] = useState(false);
+    const [isUsernameValid, setIsUsernameValid] = useState(false);
+
+    useEffect(() => {
+        setIsUsernameValid(username.length > 4 && username !== user?.user_metadata?.display_name);
+    }, [username]);
+
+    const handleSave = async () => {
+        setLoading(true);
+        const { error } = await supabase.auth.updateUser({
+            data: {
+                display_name: username,
+            }
+        })
+        if (error) {
+            Alert.alert("Error", "Failed to update username");
+        } else {
+            Alert.alert("Success", "Username updated successfully");
+            router.back();
+        }
+        setLoading(false);
+    }
+
     return (
         <View style={styles.container}>
             <Text style={styles.formTitle}>Edit Account</Text>
@@ -16,6 +44,19 @@ export default function EditAccountScreen() {
                     <View style={styles.editAvatarButton}>
                         <MaterialIcons name="edit" size={24} color="white" />
                     </View>
+                </TouchableOpacity>
+                <View style={styles.formGroup}>
+                    <Text style={styles.formGroupLabel}>Username</Text>
+                    <TextInput
+                        style={styles.input}
+                        value={username}
+                        onChangeText={setUsername}
+                        placeholder="e.g: John Doe"
+                        placeholderTextColor="gray"
+                    />
+                </View>
+                <TouchableOpacity style={[styles.saveButton, { backgroundColor: isLoading || !isUsernameValid ? "gray" : Colors.primary }]} onPress={handleSave} disabled={isLoading || !isUsernameValid}>
+                    <Text style={styles.saveButtonText}>{isLoading ? "Saving..." : "Save"}</Text>
                 </TouchableOpacity>
             </View>
         </View>
@@ -61,5 +102,30 @@ const styles = StyleSheet.create({
         backgroundColor: Colors.primary,
         borderRadius: '100%',
         padding: 8,
+    },
+    formGroup: {
+        width: "100%",
+        gap: 5,
+    },
+    formGroupLabel: {
+        fontSize: 16,
+        fontWeight: "bold",
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: "gray",
+        borderRadius: 10,
+        padding: 10,
+    },
+    saveButton: {
+        backgroundColor: Colors.primary,
+        borderRadius: 10,
+        padding: 10,
+        width: "100%",
+    },
+    saveButtonText: {
+        textAlign: "center",
+        color: "white",
+        fontWeight: "bold",
     },
 });
