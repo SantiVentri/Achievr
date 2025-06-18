75ff2cbeda8e375e9df45ad4d83b2d9ada61c789
Santino Ventrice <santino.ventrice@gmail.com>
2025-06-18 01:28:53 -0300

    Creating Home Screen titles

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -2,22 +2,41 @@ import { useUser } from '@/context/UserContext';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const getGreeting = () => {
+  const hour = new Date().getHours();
+  if (hour > 5 && hour <= 12) return "Good Morning"
+  if (hour >= 13 && hour <= 19) return "Good Afternoon"
+  return "Good Evening"
+};
+
 export default function Page() {
   const { user } = useUser();
 
   return (
     <View style={styles.container}>
-      <Text>Hola, {user?.email}</Text>
+      <View style={styles.header}>
+        <Text style={styles.subtitle}>Hi, {user?.user_metadata?.display_name}</Text>
+        <Text style={styles.title}>{getGreeting()}</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 40,
+    paddingTop: 25,
+    paddingHorizontal: 25,
     gap: 20,
+  },
+  header: {
+    gap: 5,
+  },
+  subtitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
   },
 });
