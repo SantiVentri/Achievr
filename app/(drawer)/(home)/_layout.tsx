0ca07530eaadf46642c2eff9af60a9eb8a604a82
Santino Ventrice <santino.ventrice@gmail.com>
2025-06-22 14:52:54 -0300

    Updating Home Layout

--- app/(drawer)/(home)/_layout.tsx
+++ app/(drawer)/(home)/_layout.tsx
@@ -26,7 +26,7 @@ export default function HomeLayout() {
         <Stack screenOptions={{ contentStyle: { backgroundColor: "white" } }}>
             <Stack.Screen name="index" options={{ header: () => <Header title="Dreamr" /> }} />
             <Stack.Screen name="account" options={{ headerShown: false }} />
-            <Stack.Screen name="createGoal" options={{ headerShown: false }} />
+            <Stack.Screen name="createGoal" options={{ headerShown: false, presentation: 'modal' }} />
             <Stack.Screen name="editAccount" options={{ headerShown: false }} />
             <Stack.Screen name="AvatarModal" options={{ headerShown: false, presentation: 'modal' }} />
             <Stack.Screen name="(goals)/[id]" options={{ headerShown: false }} />
