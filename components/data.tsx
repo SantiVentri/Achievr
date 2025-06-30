3cf7b8b87267a88b84306ede52bc6f239fefc2f9
Santino Ventrice <santino.ventrice@gmail.com>
2025-06-30 02:36:09 -0300

    Change goals ordering to ascending by creation date

--- components/data.tsx
+++ components/data.tsx
@@ -6,7 +6,7 @@ export async function getGoals(user: User | null, is_done: boolean): Promise<Goa
     const { data, error } = await supabase
         .from("goals")
         .select("*")
-        .order("created_at", { ascending: false })
+        .order("created_at", { ascending: true })
         .eq("creator_id", user?.id)
         .eq("is_done", is_done);
 
