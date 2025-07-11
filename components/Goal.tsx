d762501f77c83c91cc1e9ed4f766df955e1ee297
Santino Ventrice <santino.ventrice@gmail.com>
2025-07-11 03:20:16 -0300

    Updating Goal Component and Starred feature

--- components/Goal.tsx
+++ components/Goal.tsx
@@ -2,10 +2,10 @@ import { Colors } from "@/constants/palette";
 import { GoalType } from "@/enums/types";
 import { useFocusEffect, useRouter } from "expo-router";
 import { useCallback, useState } from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { getProgress } from "./data";
 
-export default function Goal({ id, icon, title, short_description, is_done }: GoalType) {
+export default function Goal({ id, header_image, icon, title, short_description, is_done, is_starred }: GoalType) {
     const router = useRouter();
     const [isLoading, setIsLoading] = useState(false);
     const [totalTasks, setTotalTasks] = useState(0);
@@ -33,17 +33,27 @@ export default function Goal({ id, icon, title, short_description, is_done }: Go
 
     return (
         <TouchableOpacity style={[styles.container, is_done && styles.is_done]} onPress={handlePress} disabled={isLoading}>
-            <Text style={styles.icon}>{icon}</Text>
-            <View style={styles.content}>
-                <View style={styles.text_container}>
-                    <Text style={[styles.title, is_done && styles.is_done_text]} numberOfLines={1} ellipsizeMode="tail">{title}</Text>
-                    <Text style={[styles.description, is_done && styles.is_done_text]} numberOfLines={3} ellipsizeMode="tail">{short_description}</Text>
+            {is_starred && (
+                <View style={styles.imageContainer}>
+                    <Image
+                        source={{ uri: header_image }}
+                        height={120}
+                    />
                 </View>
-                <View style={styles.progress}>
-                    <View style={styles.progressBar}>
-                        <View style={[styles.progressBarFill, { width: `${progress}%` }]} />
+            )}
+            <View style={styles.itemContent}>
+                <Text style={styles.icon}>{icon}</Text>
+                <View style={styles.content}>
+                    <View style={styles.text_container}>
+                        <Text style={[styles.title, is_done && styles.is_done_text]} numberOfLines={1} ellipsizeMode="tail">{title}</Text>
+                        {short_description && <Text style={[styles.description, is_done && styles.is_done_text]} numberOfLines={3} ellipsizeMode="tail">{short_description}</Text>}
                     </View>
-                    <Text style={styles.progressText}>{`${progress}%`}</Text>
+                    <View style={styles.progress}>
+                        <View style={styles.progressBar}>
+                            <View style={[styles.progressBarFill, { width: `${progress}%` }]} />
+                        </View>
+                        <Text style={styles.progressText}>{`${progress}%`}</Text>
+                    </View>
                 </View>
             </View>
         </TouchableOpacity>
@@ -52,14 +62,22 @@ export default function Goal({ id, icon, title, short_description, is_done }: Go
 
 const styles = StyleSheet.create({
     container: {
-        flexDirection: "row",
         backgroundColor: "#F4F4F4",
         borderWidth: 4,
         borderColor: Colors.primary,
         borderRadius: 15,
-        padding: 15,
-        gap: 15,
+        padding: 10,
         marginBottom: 15,
+        gap: 10
+    },
+    itemContent: {
+        flexDirection: "row",
+        padding: 5,
+        gap: 15,
+    },
+    imageContainer: {
+        borderRadius: 10,
+        overflow: 'hidden'
     },
     is_done: {
         backgroundColor: "#BCC6EB",
