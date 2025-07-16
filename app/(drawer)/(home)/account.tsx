6771df191c14cee7d36c11c7128e4767eed59226
Santino Ventrice <santino.ventrice@gmail.com>
2025-07-16 20:15:48 -0300

    Adding feedback section to Account screen

--- app/(drawer)/(home)/account.tsx
+++ app/(drawer)/(home)/account.tsx
@@ -1,18 +1,23 @@
 import Avatar from "@/components/Account/Avatar";
+import FeedbackForm from "@/components/Account/Feedback";
 import AccountSection from "@/components/Account/Section";
 import LocaleSelect from "@/components/Locale/LocaleSelect";
 import { Colors } from "@/constants/palette";
 import { useUser } from "@/context/UserContext";
+import { FontAwesome6 } from "@expo/vector-icons";
 import Feather from '@expo/vector-icons/Feather';
 import { useRouter } from "expo-router";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { useTranslation } from "react-i18next";
 import { KeyboardAvoidingView, Platform, Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function AccountScreen() {
+    const { t } = useTranslation();
     const { user } = useUser();
     const username = user?.user_metadata.display_name;
     const router = useRouter();
     const scrollViewRef = useRef<ScrollView | null>(null);
+    const [isFeedbackModalVisible, setIsFeedbackModalVisible] = useState(false);
 
     const scrollToInput = (inputY: number) => {
         scrollViewRef.current?.scrollTo({ y: inputY - 30, animated: true });
@@ -46,11 +51,22 @@ export default function AccountScreen() {
                     </View>
                 </View>
                 <View style={styles.sectionContainer}>
+                    <Pressable style={styles.feedbackSection} onPress={() => setIsFeedbackModalVisible(true)}>
+                        <FontAwesome6 name="book-bookmark" size={35} color="white" />
+                        <View style={styles.content}>
+                            <Text style={styles.feedbackTitle} >{t('feedback.title')}</Text>
+                            <Text style={styles.feedbackDescription} >{t('feedback.description')}</Text>
+                        </View>
+                    </Pressable>
                     <AccountSection type="username" onInputFocus={scrollToInput} />
                     <AccountSection type="email" onInputFocus={scrollToInput} />
                     <AccountSection type="danger" />
                 </View>
             </ScrollView>
+            <FeedbackForm
+                visible={isFeedbackModalVisible}
+                onClose={() => setIsFeedbackModalVisible(false)}
+            />
         </KeyboardAvoidingView>
     )
 }
@@ -102,5 +118,27 @@ const styles = StyleSheet.create({
     },
     sectionContainer: {
         gap: 15
+    },
+    feedbackSection: {
+        flexDirection: 'row',
+        backgroundColor: Colors.primary,
+        borderRadius: 15,
+        padding: 20,
+        gap: 20
+    },
+    content: {
+        flex: 1,
+        paddingBottom: 20,
+        gap: 5
+    },
+    feedbackTitle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'white'
+    },
+    feedbackDescription: {
+        fontSize: 15,
+        color: 'white',
+        opacity: 0.8
     },
 })
