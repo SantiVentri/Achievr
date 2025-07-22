2de362a3d2b5eab773f6c121e29b760fee9180cb
Santino Ventrice <santino.ventrice@gmail.com>
2025-07-22 00:15:46 -0300

    Updating Supabase key env variable in client config

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -4,7 +4,7 @@ import 'react-native-url-polyfill/auto'
 
 export const supabase = createClient(
   process.env.EXPO_PUBLIC_SUPABASE_URL || "",
-  process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "",
+  process.env.EXPO_PUBLIC_SUPABASE_KEY || "",
   {
     auth: {
       storage: AsyncStorage,
