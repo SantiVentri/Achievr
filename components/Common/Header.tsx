fd0ab141f701056381f0cc676f0a4bbc32aa9f36
Santino Ventrice <santino.ventrice@gmail.com>
2025-07-22 21:33:25 -0300

    Adding onPress function to title in Header

--- components/Common/Header.tsx
+++ components/Common/Header.tsx
@@ -2,15 +2,16 @@ import { Colors } from "@/constants/palette";
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { DrawerNavigationProp } from "@react-navigation/drawer";
 import { useNavigation } from "@react-navigation/native";
-import { useRouter } from "expo-router";
+import { usePathname, useRouter } from "expo-router";
 import { useState } from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Pressable, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import Avatar from "../Account/Avatar";
 
 export default function Header() {
     const router = useRouter();
     const [isLoading, setIsLoading] = useState(false);
     const navigation = useNavigation<DrawerNavigationProp<any>>();
+    const pathname = usePathname();
 
     const handleOpenDrawer = () => {
         setIsLoading(true);
@@ -18,12 +19,19 @@ export default function Header() {
         setIsLoading(false);
     }
 
+    const handleGoHome = () => {
+        if (pathname == '/') return null;
+        router.replace('/')
+    }
+
     return (
         <View style={styles.container}>
             <TouchableOpacity onPress={handleOpenDrawer} disabled={isLoading}>
                 <MaterialIcons name="menu" size={35} color="white" />
             </TouchableOpacity>
-            <Text style={styles.title}>Achievr</Text>
+            <Pressable onPress={handleGoHome}>
+                <Text style={styles.title}>Achievr</Text>
+            </Pressable>
             <TouchableOpacity onPress={() => router.push("/account")}>
                 <Avatar size={35} />
             </TouchableOpacity>
